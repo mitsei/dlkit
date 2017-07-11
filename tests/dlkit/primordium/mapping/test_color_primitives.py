55bd6a5c0c05a34fee9d4d837d6a5e81d8cee064
cjshaw <cjshaw@mit.edu>
2017-07-11 16:08:10 -0400

    with color primitive mapping tests

--- tests/dlkit/primordium/mapping/test_color_primitives.py
+++ tests/dlkit/primordium/mapping/test_color_primitives.py
@@ -5,6 +5,12 @@ from dlkit.primordium.mapping.color_primitives import RGBColorCoordinate
 from dlkit.primordium.type.primitives import Type
 
 
+@pytest.fixture(scope="function")
+def color_coordinate_test_wrapper(request):
+    request.cls.color = RGBColorCoordinate(values=[1, 250, 69])
+
+
+@pytest.mark.usefixtures("color_coordinate_test_wrapper")
 class TestRGBColorCoordinate(object):
     def test_can_create_color_with_hexstr(self):
         result = RGBColorCoordinate(hexstr='32fa45')
@@ -16,60 +22,76 @@ class TestRGBColorCoordinate(object):
 
     def test_hexstr_throws_invalid_argument_for_wrong_length(self):
         with pytest.raises(errors.InvalidArgument):
-            RGBColorCoordinate(hexstr='0011001100')
+            RGBColorCoordinate(hexstr='0011001')
+        with pytest.raises(errors.InvalidArgument):
+            RGBColorCoordinate(hexstr='00110')
 
     def test_hexstr_throws_invalid_argument_for_non_hex(self):
-        assert False
+        with pytest.raises(errors.InvalidArgument):
+            RGBColorCoordinate(hexstr='0011zz')
 
     def test_values_throws_invalid_argument_for_non_list(self):
-        assert False
+        with pytest.raises(errors.InvalidArgument):
+            RGBColorCoordinate(values='32fa45')
 
     def test_values_throws_invalid_argument_for_wrong_length(self):
-        assert False
+        with pytest.raises(errors.InvalidArgument):
+            RGBColorCoordinate(values=[1, 2, 3, 4])
+        with pytest.raises(errors.InvalidArgument):
+            RGBColorCoordinate(values=[1, 2])
 
     def test_can_create_color_with_values(self):
-        result = DisplayText(text='foo',
-                             language_type=Type('639-2%3AHIN%40ISO'),
-                             format_type=Type('TextFormats%3APLAIN%40okapia.net'),
-                             script_type=Type('15924%3ADEVA%40ISO'))
-        assert result._text == 'foo'
-        assert isinstance(result._language_type, Type)
-        assert str(result._language_type) == '639-2%3AHIN%40ISO'
-        assert isinstance(result._format_type, Type)
-        assert str(result._format_type) == 'TextFormats%3APLAIN%40okapia.net'
-        assert isinstance(result._script_type, Type)
-        assert str(result._script_type) == '15924%3ADEVA%40ISO'
+        result = RGBColorCoordinate(values=[50, 250, 69])
+        assert result._values == [50, 250, 69]
 
     def test_can_create_color_with_uncertainty_minus(self):
-        assert False
+        result = RGBColorCoordinate(values=[50, 250, 69],
+                                    uncertainty_minus=[5, 5, 5])
+        assert result._uncertainty_minus == [5, 5, 5]
 
     def test_can_create_color_with_uncertainty_plus(self):
-        assert False
+        result = RGBColorCoordinate(values=[50, 250, 69],
+                                    uncertainty_plus=[15, 15, 15])
+        assert result._uncertainty_plus == [15, 15, 15]
 
     def test_initer_raises_null_argument(self):
         with pytest.raises(errors.NullArgument):
-            DisplayText()
+            RGBColorCoordinate()
 
     def test_can_get_string_representation(self):
-        assert False
+        assert str(self.color) == '01fa45'
 
     def test_can_get_coordinate_type(self):
-        assert self.display_text.text == 'foo'
+        assert isinstance(self.color.coordinate_type, Type)
+        assert str(self.color.coordinate_type) == 'mapping.Coordinate%3Argb_color%40ODL.MIT.EDU'
 
     def test_can_get_dimensions(self):
-        assert isinstance(self.display_text.language_type, Type)
-        assert str(self.display_text.language_type) == '639-2%3AHIN%40ISO'
+        assert self.color.dimensions == 3
 
     def test_can_get_values(self):
-        assert isinstance(self.display_text.script_type, Type)
-        assert str(self.display_text.script_type) == '15924%3ADEVA%40ISO'
+        assert self.color.values == [1, 250, 69]
 
     def test_can_check_defines_uncertainty(self):
-        assert isinstance(self.display_text.format_type, Type)
-        assert str(self.display_text.format_type) == 'TextFormats%3APLAIN%40okapia.net'
+        assert not self.color.defines_uncertainty()
+
+        result = RGBColorCoordinate(values=[50, 250, 69],
+                                    uncertainty_plus=[15, 15, 15])
+        assert result.defines_uncertainty()
+
+        result = RGBColorCoordinate(values=[50, 250, 69],
+                                    uncertainty_minus=[5, 5, 5])
+        assert result.defines_uncertainty()
 
     def test_can_get_uncertainty_plus(self):
-        assert False
+        assert self.color.uncertainty_plus is None
+
+        result = RGBColorCoordinate(values=[50, 250, 69],
+                                    uncertainty_plus=[15, 15, 15])
+        assert result.uncertainty_plus == [15, 15, 15]
 
     def test_can_get_uncertainty_minus(self):
-        assert False
+        assert self.color.uncertainty_minus is None
+
+        result = RGBColorCoordinate(values=[50, 250, 69],
+                                    uncertainty_minus=[5, 5, 5])
+        assert result.uncertainty_minus == [5, 5, 5]
