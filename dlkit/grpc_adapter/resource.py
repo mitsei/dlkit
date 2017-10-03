39d5ec544b40a1d62432b251412cdd71f2bf2077
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-10-03 14:34:15 -0400

    updated the grpc_adapter with runtime

--- dlkit/grpc_adapter/resource.py
+++ dlkit/grpc_adapter/resource.py
@@ -1,21 +1,71 @@
 from dlkit.json_.resource import sessions as json_resource_sessions
+from dlkit.primordium.id import primitives_pb2 as id_primitives
+from dlkit.primordium.locale import primitives_pb2 as locale_primitives
+from dlkit.primordium.type import primitives_pb2 as type_primitives
 from dlkit.proto import resource_pb2
 from dlkit.proto import resource_pb2_grpc
 
 
 class BinLookupSessionServicer(resource_pb2_grpc.BinLookupSessionServicer,
                                json_resource_sessions.BinLookupSession):
-    # def __init__(self):
-    #     # Deal with runtime and initialization somehow?
-    #     pass
+    def __init__(self, runtime):
+        # Deal with runtime and initialization somehow?
+        self._runtime = runtime
+        self._catalog_session = None
+        self._proxy = None
 
     def GetBins(self, request, context):
-        bins = self.get_bins()
-        for bin_ in bins:
-            import pdb
-            pdb.set_trace()
-            yield resource_pb2.Bin(displayName=bin_.display_name,
-                                   description=bin_.description,
-                                   id=bin_.ident,
-                                   genusTypeId=bin_.genus_type,
-                                   recordTypeIds=bin_.record_types)
+        for bin_ in self.get_bins():
+            yield resource_pb2.Bin(
+                displayName=locale_primitives.DisplayText(
+                    text=bin_.display_name.text,
+                    format_type_id=type_primitives.Type(
+                        authority=bin_.display_name.format_type.authority,
+                        identifier=bin_.display_name.format_type.identifier,
+                        namespace=bin_.display_name.format_type.namespace
+                    ),
+                    language_type_id=type_primitives.Type(
+                        authority=bin_.display_name.language_type.authority,
+                        identifier=bin_.display_name.language_type.identifier,
+                        namespace=bin_.display_name.language_type.namespace
+                    ),
+                    script_type_id=type_primitives.Type(
+                        authority=bin_.display_name.script_type.authority,
+                        identifier=bin_.display_name.script_type.identifier,
+                        namespace=bin_.display_name.script_type.namespace
+                    )
+                ),
+                description=locale_primitives.DisplayText(
+                    text=bin_.description.text,
+                    format_type_id=type_primitives.Type(
+                        authority=bin_.description.format_type.authority,
+                        identifier=bin_.description.format_type.identifier,
+                        namespace=bin_.description.format_type.namespace
+                    ),
+                    language_type_id=type_primitives.Type(
+                        authority=bin_.description.language_type.authority,
+                        identifier=bin_.description.language_type.identifier,
+                        namespace=bin_.description.language_type.namespace
+                    ),
+                    script_type_id=type_primitives.Type(
+                        authority=bin_.description.script_type.authority,
+                        identifier=bin_.description.script_type.identifier,
+                        namespace=bin_.description.script_type.namespace
+                    )
+                ),
+                id=id_primitives.Id(
+                    authority=bin_.ident.authority,
+                    identifier=bin_.ident.identifier,
+                    namespace=bin_.ident.namespace
+                ),
+                genusTypeId=type_primitives.Type(
+                    authority=bin_.genus_type.authority,
+                    identifier=bin_.genus_type.identifier,
+                    namespace=bin_.genus_type.namespace
+                ),
+                recordTypeIds=[type_primitives.Type(
+                    authority=record_type.authority,
+                    identifier=record_type.identifier,
+                    namespace=record_type.namespace
+                ) for record_type in bin_.record_types]
+            )
--- test_grpc_resource_server.py
+++ test_grpc_resource_server.py
@@ -5,16 +5,28 @@ import time
 
 import grpc
 
-from dlkit.proto import resource_pb2_grpc
 from dlkit.grpc_adapter.resource import BinLookupSessionServicer
+from dlkit.proto import resource_pb2_grpc
+from dlkit.runtime import PROXY_SESSION, RUNTIME
+from dlkit.runtime.proxy_example import SimpleRequest
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+condition = PROXY_SESSION.get_proxy_condition()
+dummy_request = SimpleRequest(username='grpc@test.com',
+                              authenticated=True)
+condition.set_http_request(dummy_request)
+
+proxy = PROXY_SESSION.get_proxy(condition)
+mgr = RUNTIME.get_service_manager('RESOURCE',
+                                  proxy=proxy)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     resource_pb2_grpc.add_BinLookupSessionServicer_to_server(
-        BinLookupSessionServicer(), server)
+        BinLookupSessionServicer(mgr._runtime), server)
 
     server.add_insecure_port('[::]:50051')
     server.start()
